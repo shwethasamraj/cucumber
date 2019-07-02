36a83f279f434235572245cd4cf7bc3b0269df46
shwethasamraj <52441892+shwethasamraj@users.noreply.github.com>
2019-07-02 16:19:46 +0530

    Update RunnerClass.java

--- src/test/java/Runner/RunnerClass.java
+++ src/test/java/Runner/RunnerClass.java
@@ -5,7 +5,7 @@ import org.junit.runner.RunWith;
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
-@CucumberOptions(features={"src/main/resources/datatable.feature"},glue="stepdefinition",plugin="html:target/htmlreport")
+@CucumberOptions(features={"src/main/resources/datatable.feature"},glue="stepdefinition",plugin="json:target/jsonreport")
 public class RunnerClass {
 
 }
